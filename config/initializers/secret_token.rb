b80c4b6152b7fb03745f250fcdaea223d4ebdee0
Alex Tang <lalextang@msn.com>
2011-01-06 21:14:19 +0800

    empty project with root/password mysql config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrumBook::Application.config.secret_token = 'fb67043df1d11cb290fe6da3b9db08eb0001a40313ae20952b635786e2f901bc694e563c8a595acbebd38c7d6331e2e9271f8bb07090bb57f9b2f581c4fd1f98'
