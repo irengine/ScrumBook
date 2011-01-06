7603be897a175908d7a7a8a16ca319d5151b2d09
Alex Tang <lalextang@msn.com>
2011-01-07 01:18:15 +0800

    T1-2# add User model, fixture and unit test

---
+++ db/migrate/20110106164707_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name, :null=>false
+      t.string :full_name
+      t.string :email, :null=>false
+      t.string :hashed_password, :null=>false
+      t.string :salt, :null=>false
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
