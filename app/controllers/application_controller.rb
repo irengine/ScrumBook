a51ad6b7f898f2829c0552f441c8e90c22476bcb
zyh <zhangyh@taoware.com>
2011-01-10 16:56:04 +0800

    修改 用户登录 添加 用户管理
    -------------------------------------
    没有添加 测试

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def getSessionByKey(key)
+    return session[key];
+  end
+
+  def writeSession(key,values)
+    session[key]=values;
+  end
+
+  def getUserName()
+    return session["UserName"]
+  end
+
+  def getUserId()
+    return getSessionByKey("UserId")
+  end
+
+  def getUserProductCode()
+     return getSessionBykey("ProductCode") 
+  end
+
+  def login
+     if (session["UserId"].nil? or session['UserId']=="")
+       redirect_to :controller=> 'welcomes', :action=> 'index'
+     end
+  end
+
 end
