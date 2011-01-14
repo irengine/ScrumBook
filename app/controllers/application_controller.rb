91e3d4364e80c7859ce95db2bb58195ade34312a
zyh <zhangyh@taoware.com>
2011-01-14 13:02:18 +0800

    修整文件

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,13 @@ class ApplicationController < ActionController::Base
   def get_UserProductCode()
      return get_Session_Key("ProductCode") 
   end
+    def get_UserProductName()
+     return get_Session_Key("ProductName")
+  end
+  
+  def get_UserProductId()
+     return get_Session_Key("ProductId")
+  end
 
   def login
      if (session["UserId"].nil? or session['UserId']=="")
