4b35e5d7bd3614f720cbbf0d93cfd795c1b16b17
zyh <zhangyh@taoware.com>
2011-01-14 12:05:55 +0800

    T1-4# 修改用户管理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,49 +33,30 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-
-      if @user.save
-         redirect_to(:action=>'index')
-      else
-        redirect_to( :action => "new" ,:Messages =>"用户添加失败!")
-      end
+    if @user.save
+      redirect_to(:controller=>"users",:action=>'index')
+    else
+      flash[:notice]=I18n.t(:user_add_error)
+      redirect_to(:controller=>"users",:action=>'new')
+    end
   end
 
   def update
-    
     @user=User.find_by_id(params[:id])
-
-    if(params[:id]==getUserId().to_s)
-      @oldPwd=params["oldPwd"]
-      @newPwd=params["newPwd"]
-      @cfmPwd=params["cfmPwd"]
-      if(@oldPwd!=nil && @oldPwd.length>0)
-        @user=User.authenticate(getUserName(),@oldPwd)
-        if(@user!=nil)
-          if(@newPwd!=@cfmPwd)
-            redirect_to(:action=>'edit',:Messages => '两次输入的密码不一样!')
-            return
-          else
-            @user.password=@newPwd
-          end
-        else
-          redirect_to(:action=>'edit',:Messages => '密码验证失败!')
+    if(params[:id]==get_UserId().to_s)
+      @old_pwd=params[:old_pwd]
+      if(@old_pwd.nil? && @old_pwd.empty?)
+        @user=User.authenticate(get_UserName(),@old_pwd)
+        if(@user.nil?)
+          redirect_to(:action=>'edit',:messages =>  I18n.t(:user_authenticate_error))
           return
         end
       end
-    else
-      @password=params["UserPassword"]
-      if(@password!=nil && @password.length>0)
-         @user.password=@password
-      end
     end
-
-    @user.email=params["email"]
-    @user.full_name=params["full_name"]
-    if (@user.save())
-       redirect_to(:action=>'edit',:Messages => '信息保存成功!')
+    if (@user.update_attributes(params[:user]))
+       redirect_to(:action=>'edit',:messages => I18n.t(:user_update_succeed))
     else
-        redirect_to(:action=>'edit',:Messages => '信息保存失败!')
+        redirect_to(:action=>'edit',:messages => I18n.t(:user_update_error))
     end
   end
 end
